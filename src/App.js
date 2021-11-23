94c7235e119cb4a843babf1afddca2ea98190bfe
zubxxr <zubxxr@gmail.com>
2021-11-23 16:10:54 -0500

    yeee

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import NavBar from './NavigationBar/NavBar.js'
 import Home from './components/Home/Home.js'
 import About from './components/About/About.js'
 import Contact from './components/Contact/Contact.js'
-import Cart from './components/Cart/Cart.js'
-import Footer from './components/Footer/webFooter.js'
+import Cart from './CartComponents/Cart/Cart'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 //import SignUpForm from './JavaScript/SignUp/SignUpForm.js'
 //import SignIn from './JavaScript/SignUp/Login.js'
@@ -16,9 +15,12 @@ import Logout from './UserAuthentication/Logout';
 import { useState, useEffect } from "react";
 import { AuthContext } from './helpers/AuthContext';
 import axios from 'axios';
+import Products from './CartComponents/Products/Products';
+import { connect } from "react-redux";
+import Footer from './components/Footer/webFooter';
 import Profile from './components/Profile/Profile';
 
-const App = () => {
+const App = ({ current }) => {
   const [authState, setAuthState] = useState({
     username: "",
     id: 0,
@@ -36,32 +38,38 @@ const App = () => {
 
           {/*Navbar Component */}
           <NavBar />
-            <Routes className="routes-container">
-              <Route path="/" exact element={<Home />} />
-              <Route path="/About" element={<About />} />
+          <Routes className="routes-container">
+            <Route path="/" exact element={<Home />} />
+            <Route path="/About" element={<About />} />
 
-              <Route path="/RegistrationTester" element={<RegistrationTester />} />
-              <Route path="/LoginTester" element={<LoginTester />} />
+            <Route path="/RegistrationTester" element={<RegistrationTester />} />
+            <Route path="/LoginTester" element={<LoginTester />} />
 
-              <Route path="/Logout" element={<Logout />} />
- <Route path="/RegistrationTester" element={<RegistrationTester />} />
+            <Route path="/Logout" element={<Logout />} />
+
+            <Route path="/ContactUs" element={<Contact />} />
+            <Route path="/Cart" element={<Cart />} />
+
+
+            <Route path="/RegistrationTester" element={<RegistrationTester />} />
             <Route path="/LoginTester" element={<LoginTester />} />
             <Route path="/profile" element={<Profile />} />
 
-              <Route path="/ContactUs" element={<Contact />} />
-              <Route path="/Cart" element={<Cart />} />
-            </Routes>
+            <Route path="/ContactUs" element={<Contact />} />
+            <Route path="/Cart" element={<Cart />} />
+            <Route path="/Products" element={<Products />} />
+          </Routes>
           <Footer />
-
-           {/* <Route path="/RegistrationTester" element={<RegistrationTester />} />
-            <Route path="/LoginTester" element={<LoginTester />} />
-            <Route path="/profile" element={<Profile />} /> */}
-
         </div>
       </Router>
     </AuthContext.Provider>
 
   );
 }
+const mapStateToProps = (state) => {
+  return {
+    current: state.shop.currentItem,
+  };
+};
 
-export default App;
+export default connect(mapStateToProps)(App);
--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,13 +1,21 @@
 // About.js will contain the about section
 
-import background from '../../images/cityeditted.jpg';
 import '../About/About.css';
+import background from '../../images/cityeditted.jpg';
+
+
+import '../About/About.css';
+
+import './About.css';
+//import background from '../images/cityeditted.jpg';
+
 
 
 const About = () => {
     return (
         <div className="about-container">
 
+            <img src={background} alt="" />
             {/*<img src={background} alt=""/>*/}
 
             <div className="about">
@@ -18,11 +26,11 @@ const About = () => {
             </div>
 
             <div className="description">
-                <p>H2Z2 is a online grocery store that allows its customers to order any and all types of foods and vegetables that everyone needs on a daily basis. 
+                <p>H2Z2 is a online grocery store that allows its customers to order any and all types of foods and vegetables that everyone needs on a daily basis.
                     At H2Z2 we put our customers above all else and will do anything to ensure that customers have the easiest and fattest way to shop for their daily groceries that will be delivered right to their doorstep.
                     H2Z2 gets fresh vegetables and fruits every single day to endure that what we sell to our customers in only the best of the best. Our bakery goods are made fresh every single morning andmore throuout the day for 24/7 freshness.
-                    As a Web Programming project this website was created to be an engaging website where customers can interact with. This website has a contact us page and signup/login page that 
-                    is connected to a database that saves all the information that is inputted and also shows the message if someone used the contact us page. 
+                    As a Web Programming project this website was created to be an engaging website where customers can interact with. This website has a contact us page and signup/login page that
+                    is connected to a database that saves all the information that is inputted and also shows the message if someone used the contact us page.
                 </p>
 
                 <p></p>
@@ -36,12 +44,12 @@ const About = () => {
                 <h1>Staff Descriptions</h1>
             </div>
 
-            {/*Staff descriptions below*/ }
+            {/*Staff descriptions below*/}
 
             <div className="huzaifa-img">
             </div>
             <div className="huzaifa">
-                <p>Hello, my name is Hanzalah Patel! I am a second year student at Ontario Tech University studying Software engineering. This website was designed as the final project for 
+                <p>Hello, my name is Hanzalah Patel! I am a second year student at Ontario Tech University studying Software engineering. This website was designed as the final project for
                     my Web Programming class.
                 </p>
             </div>
@@ -49,7 +57,7 @@ const About = () => {
             <div className="hanzalah-img">
             </div>
             <div className="hanzalah">
-                <p>Hello, my name is Hanzalah Patel! I am a second year student at Ontario Tech University studying Software engineering. This website was designed as the final project for 
+                <p>Hello, my name is Hanzalah Patel! I am a second year student at Ontario Tech University studying Software engineering. This website was designed as the final project for
                     my Web Programming class.
                 </p>
             </div>
@@ -57,7 +65,7 @@ const About = () => {
             <div className="zubair-img">
             </div>
             <div className="zubair">
-                <p>Hello, my name is Hanzalah Patel! I am a second year student at Ontario Tech University studying Software engineering. This website was designed as the final project for 
+                <p>Hello, my name is Hanzalah Patel! I am a second year student at Ontario Tech University studying Software engineering. This website was designed as the final project for
                     my Web Programming class.
                 </p>
             </div>
@@ -65,7 +73,7 @@ const About = () => {
             <div className="ziyad-img">
             </div>
             <div className="ziyad">
-            <p>Hello, my name is Hanzalah Patel! I am a second year student at Ontario Tech University studying Software engineering. This website was designed as the final project for 
+                <p>Hello, my name is Hanzalah Patel! I am a second year student at Ontario Tech University studying Software engineering. This website was designed as the final project for
                     my Web Programming class.
                 </p>
             </div>
